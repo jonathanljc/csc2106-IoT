9e7699a3224ccf605d81b132a53078110644b699
Felix Chang <2301105@sit.singaporetech.edu.sg>
2025-03-02 16:09:57 +0800

    clean the cli main.c to serve as a template to add udp functions

--- cli/src/main.c
+++ cli/src/main.c
@@ -6,17 +6,16 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
-#if defined(CONFIG_CLI_SAMPLE_MULTIPROTOCOL)
-#include "ble.h"
-#endif
-
-#if defined(CONFIG_CLI_SAMPLE_LOW_POWER)
-#include "low_power.h"
-#endif
-
+#include <openthread/platform/radio.h>
+#include <openthread/thread.h>
+#include <openthread/udp.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/usb/usb_device.h>
+
+static const char UDP_DEST_ADDR[] = "fd97:b3f:1a22:1:7ef1:9f0f:c300:f9fc";  // IPv6 Address of OTBR
+
+static const uint16_t UDP_PORT = 1234;  // Choose a suitable UDP port
+
 
 LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 
@@ -32,51 +31,7 @@ LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 
 int main(void)
 {
-#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
-	int ret;
-	const struct device *dev;
-	uint32_t dtr = 0U;
-
-	ret = usb_enable(NULL);
-	if (ret != 0 && ret != -EALREADY) {
-		LOG_ERR("Failed to enable USB");
-		return 0;
-	}
-
-	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
-	if (dev == NULL) {
-		LOG_ERR("Failed to find specific UART device");
-		return 0;
-	}
-
-	LOG_INF("Waiting for host to be ready to communicate");
-
-	/* Data Terminal Ready - check if host is ready to communicate */
-	while (!dtr) {
-		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-		if (ret) {
-			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
-				ret);
-			continue;
-		}
-		k_msleep(100);
-	}
-
-	/* Data Carrier Detect Modem - mark connection as established */
-	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
-	/* Data Set Ready - the NCP SoC is ready to communicate */
-	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
-#endif
-
 	LOG_INF(WELLCOME_TEXT);
-
-#if defined(CONFIG_CLI_SAMPLE_MULTIPROTOCOL)
-	ble_enable();
-#endif
-
-#if defined(CONFIG_CLI_SAMPLE_LOW_POWER)
-	low_power_enable();
-#endif
 
 	return 0;
 }
